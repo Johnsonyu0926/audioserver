48e2dc874b93912cd74ab2d593b536f10509f290
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-10-25 14:50:14 +0800

    Update ch395q_other_features.lua

--- etc/ch395q_other_features.lua
+++ etc/ch395q_other_features.lua
@@ -16,7 +16,7 @@ ch395q.set_ip(ip, netmask, gateway, dns)
 
 -- 启动HTTP服务器并重定向到HTTPS
 ch395q.start_http_server(80, function(request)
-    return "HTTP/1.1 301 Moved Permanently\r\nLocation: https://" .. request.headers["Host"] .. request.path .. "\r\n\r\n"
+    return "HTTP/1.1 301 Moved Permanently\r\nLocation: https://"..request.headers["Host"]..request.path.."\r\n\r\n"
 end)
 
 -- 启动HTTPS服务器，配置证书和私钥
@@ -25,84 +25,32 @@ local key_file = "/path/to/key.pem"
 ch395q.start_https_server(443, cert_file, key_file)
 
 -- 处理HTTPS请求
-function handle_https_request(request)
+local function handle_https_request(request)
     local response = ""
 
     if request.path == "/network" then
-        response = [[
-            <html>
-            <body>
-                <h1>Network Configuration</h1>
-                <form action="/network" method="post">
-                    IP: <input type="text" name="ip" value="]] .. ip .. [[" /><br />
-                    Netmask: <input type="text" name="netmask" value="]] .. netmask .. [[" /><br />
-                    Gateway: <input type="text" name="gateway" value="]] .. gateway .. [[" /><br />
-                    DNS: <input type="text" name="dns" value="]] .. dns .. [[" /><br />
-                    <input type="submit" value="Save" />
-                </form>
-            </body>
-            </html>
-        ]]
+        response = generate_network_form(ip, netmask, gateway, dns)
     elseif request.method == "POST" and request.path == "/network" then
-        ip = request.body.ip
-        netmask = request.body.netmask
-        gateway = request.body.gateway
-        dns = request.body.dns
+        ip, netmask, gateway, dns = update_network_config(request.body)
         ch395q.set_ip(ip, netmask, gateway, dns)
         response = "Network configuration updated"
     elseif request.path == "/audio" then
-        response = [[
-            <html>
-            <body>
-                <h1>Audio Management</h1>
-                <form action="/audio" method="post">
-                    Volume: <input type="range" name="volume" min="0" max="100" value="50" /><br />
-                    <input type="submit" value="Set Volume" />
-                </form>
-            </body>
-            </html>
-        ]]
+        response = generate_audio_form()
     elseif request.method == "POST" and request.path == "/audio" then
         local volume = request.body.volume
-        -- 设置音量
         ch395q.set_volume(volume)
-        response = "Volume set to " .. volume
+        response = "Volume set to "..volume
     elseif request.path == "/system" then
-        local system_info = ch395q.get_system_info()
-        response = [[
-            <html>
-            <body>
-                <h1>System Information</h1>
-                <p>Device ID: ]] .. system_info.device_id .. [[</p>
-                <p>Remaining Space: ]] .. system_info.remaining_space .. [[</p>
-                <p>Memory: ]] .. system_info.memory .. [[</p>
-                <p>CPU: ]] .. system_info.cpu .. [[</p>
-                <p>4G ICCD: ]] .. system_info.iccd .. [[</p>
-                <p>LBS: ]] .. system_info.lbs .. [[</p>
-                <form action="/reboot" method="post">
-                    <input type="submit" value="Reboot" />
-                </form>
-                <form action="/backup" method="post">
-                    <input type="submit" value="Backup Configuration" />
-                </form>
-                <form action="/restore" method="post">
-                    <input type="submit" value="Restore Factory Settings" />
-                </form>
-            </body>
-            </html>
-        ]]
+        response = generate_system_info()
     elseif request.method == "POST" and request.path == "/reboot" then
         ch395q.reboot()
         response = "Rebooting..."
     elseif request.method == "POST" and request.path == "/backup" then
         local backup_data = ch395q.backup_configuration()
-        -- 加密备份数据
         local encrypted_backup_data = encrypt(backup_data)
-        -- 保存备份数据
         save_backup(encrypted_backup_data)
         response = "Configuration backed up"
     elseif request.method == "POST" and request.path == "/restore" then
-        -- 恢复出厂设置
         local factory_settings = load_factory_settings()
         ch395q.restore_configuration(factory_settings)
         response = "Factory settings restored"
@@ -112,5 +60,70 @@ function handle_https_request(request)
 end
 
 ch395q.on_https_request(handle_https_request)
+
+-- 生成网络配置表单
+local function generate_network_form(ip, netmask, gateway, dns)
+    return [[
+        <html>
+        <body>
+            <h1>Network Configuration</h1>
+            <form action="/network" method="post">
+                IP: <input type="text" name="ip" value="]]..ip..[[" /><br />
+                Netmask: <input type="text" name="netmask" value="]]..netmask..[[" /><br />
+                Gateway: <input type="text" name="gateway" value="]]..gateway..[[" /><br />
+                DNS: <input type="text" name="dns" value="]]..dns..[[" /><br />
+                <input type="submit" value="Save" />
+            </form>
+        </body>
+        </html>
+    ]]
+end
+
+-- 更新网络配置
+local function update_network_config(body)
+    return body.ip, body.netmask, body.gateway, body.dns
+end
+
+-- 生成音频管理表单
+local function generate_audio_form()
+    return [[
+        <html>
+        <body>
+            <h1>Audio Management</h1>
+            <form action="/audio" method="post">
+                Volume: <input type="range" name="volume" min="0" max="100" value="50" /><br />
+                <input type="submit" value="Set Volume" />
+            </form>
+        </body>
+        </html>
+    ]]
+end
+
+-- 生成系统信息
+local function generate_system_info()
+    local system_info = ch395q.get_system_info()
+    return [[
+        <html>
+        <body>
+            <h1>System Information</h1>
+            <p>Device ID: ]]..system_info.device_id..[[</p>
+            <p>Remaining Space: ]]..system_info.remaining_space..[[</p>
+            <p>Memory: ]]..system_info.memory..[[</p>
+            <p>CPU: ]]..system_info.cpu..[[</p>
+            <p>4G ICCD: ]]..system_info.iccd..[[</p>
+            <p>LBS: ]]..system_info.lbs..[[</p>
+            <form action="/reboot" method="post">
+                <input type="submit" value="Reboot" />
+            </form>
+            <form action="/backup" method="post">
+                <input type="submit" value="Backup Configuration" />
+            </form>
+            <form action="/restore" method="post">
+                <input type="submit" value="Restore Factory Settings" />
+            </form>
+        </body>
+        </html>
+    ]]
+end
 
 -- By GST @2024/10/25
