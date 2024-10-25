bda775e4772f191e78d05f19ff342cb379b19ff9
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-10-25 14:29:12 +0800

    Update ch395q_rtmp.lua

--- etc/ch395q_rtmp.lua
+++ etc/ch395q_rtmp.lua
@@ -3,6 +3,7 @@
 
 local spi = require "spi"
 local ch395q = require "ch395q"
+require "watchdog"
 
 -- 初始化SPI接口
 spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 8, 20 * 1000 * 1000)
@@ -21,7 +22,21 @@ ch395q.set_ip(ip, netmask, gateway, dns)
 
 -- 连接RTMP服务器
 local rtmp_url = "rtmp://" .. server_ip .. "/live/stream"
-ch395q.connect_rtmp(rtmp_url)
+
+-- 判断网络状态并选择RTMP传输路径
+function connect_rtmp()
+    local network_status = ch395q.get_network_status()
+    if network_status == "LAN" then
+        -- 优先使用LAN网络传输RTMP数据
+        ch395q.connect_rtmp(rtmp_url)
+    elseif network_status == "4G" then
+        -- 如果LAN不可用，则使用4G网络传输RTMP数据
+        ch395q.connect_rtmp(rtmp_url)
+    else
+        -- 如果都不可用，尝试使用4G网络传输RTMP数据
+        ch395q.connect_rtmp(rtmp_url)
+    end
+end
 
 -- 发送RTMP数据
 function send_rtmp_data(data)
@@ -32,5 +47,29 @@ end
 function receive_rtmp_data()
     return ch395q.receive_rtmp()
 end
+
+watchdog.init(60000) -- 初始化看门狗，超时时间为60秒
+
+sys.taskInit(function()
+    sys.timerLoopStart(function()
+        watchdog.feed() -- 喂狗
+    end, 60000)
+
+    sys.timerLoopStart(function()
+        local network_status = ch395q.get_network_status()
+        if network_status == "disconnected" then
+            ch395q.reconnect()
+        end
+    end, 60000)
+
+    sys.timerLoopStart(function()
+        local cpu_usage = sys.get_cpu_usage()
+        local memory_usage = sys.get_memory_usage()
+        local storage_usage = sys.get_storage_usage()
+    end, 60000)
+
+    -- 初始化时连接RTMP服务器
+    connect_rtmp()
+end)
 
 -- By GST @2024/10/25
