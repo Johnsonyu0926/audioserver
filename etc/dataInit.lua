09ea478e0acde28d60639d60c3b1c141ab232e1e
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-10-25 14:25:18 +0800

    Update dataInit.lua

--- etc/dataInit.lua
+++ etc/dataInit.lua
@@ -1,7 +1,5 @@
 -- Filename: datainit.lua
 --- 模块功能：开机数据初始化
--- @author Mikasa
--- @module mqtt.core
 -- @release 2023.12.29
 module(..., package.seeall)
 
@@ -10,6 +8,7 @@ require "pins"
 require "config"
 require "sys"
 require "util"
+require "watchdog"
 
 nvm.init("config.lua")
 
@@ -18,9 +17,7 @@ local setGpio25Fnc = pins.setup(pio.P0_25, 0)  -- 5V
 local setGpio26Fnc = pins.setup(pio.P0_26, 0)  -- 12V
 
 function initData()
-    log.info("数据初始化模块获取网络模式: ", NETMODE, NETWORKTYPE)
     if NETMODE == 1 or NETWORKTYPE == 1 then -- mqtt
-        log.info('数据初始化模块(MQTT)')
         local relay_list = nvm.get('relay_list') or {
             { id = 1, relayMode = 1, relayStatus = 0 },
             { id = 2, relayMode = 1, relayStatus = 0 },
@@ -34,7 +31,6 @@ function initData()
         end
         util.updateRelayList(relay_list)
     else
-        log.info('数据初始化模块TCP')
         local relay_mode = nvm.get('relay_mode')
         local relay_status = nvm.get('relay_status')
         util.updateRelayStatus(0, relay_mode, relay_status)
@@ -47,24 +43,26 @@ function updateRebootTask(task)
     reboot_task = task
 end
 
+watchdog.init(60000) -- 初始化看门狗，超时时间为60秒
+
 sys.taskInit(function()
     sys.wait(30000)
     if NETMODE == 1 or NETWORKTYPE == 1 then
         sys.timerLoopStart(function()
             local t = os.date("*t", os.time())
-            log.info("printTime", string.format("%04d-%02d-%02d %02d:%02d:%02d", t.year, t.month, t.day, t.hour, t.min, t.sec))
-            log.info('reboot task: ', json.encode(reboot_task))
             local currentTime = string.format("%02d:%02d", t.hour, t.min)
             for _, val in ipairs(reboot_task) do
-                log.info('对比时间：', val, currentTime)
                 if val == currentTime then
-                    log.info('开始重启')
                     sys.restart("定时重启")
                     break
                 end
             end
         end, 60000)
     end
+
+    sys.timerLoopStart(function()
+        watchdog.feed() -- 喂狗
+    end, 60000)
 end)
 
 -- By GST @2024/10/25
