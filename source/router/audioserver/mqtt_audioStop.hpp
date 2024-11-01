f954f272ca51c929127814f8d1f0fc4270075915
Johnsonyu0926 <97774037+Johnsonyu0926@users.noreply.github.com>
2024-11-01 20:30:46 +0800

    Update mqtt_audioStop.hpp

--- source/router/audioserver/mqtt_audioStop.hpp
+++ source/router/audioserver/mqtt_audioStop.hpp
@@ -12,6 +12,45 @@
 #include <nlohmann/json.hpp>
 #include "mqtt.hpp"
 #include "audioStop.hpp"
+#include "json.hpp"
+#include "utils.h"
+
+// Filename: mqtt_audiostop.hpp
+// By GST ARMV8 GCC 13.2
+
+namespace asns {
+
+template<typename Quest, typename Result>
+class CReQuest;
+
+template<typename T>
+class CResult;
+
+class CAudioStopResultData {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CAudioStopResultData, null)
+
+    template<typename Quest, typename Result, typename T>
+    int do_success(const CReQuest<Quest, Result> &c, CResult<T> &r) {
+        AudioPlayUtil::audio_stop();
+        r.resultId = 1;
+        r.result = "success";
+        return 1;
+    }
+
+private:
+    std::nullptr_t null;
+};
+
+class CAudioStopData {
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(CAudioStopData, null)
+
+private:
+    std::nullptr_t null;
+};
+
+} // namespace asns
 
 class MqttAudioStop {
 public:
@@ -110,5 +149,4 @@ private:
     std::vector<AudioStop> audioConfig;
     std::unique_ptr<MqttClient> mqttClient;
 };
-
 //BY GST ARMV8 GCC 13.2
